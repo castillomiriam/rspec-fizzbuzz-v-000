d29df219e25a9d9764b9fc780f19cf5c20900873
Miriam Castillo <castillo.m.mc@gmail.com>
2018-02-01 06:02:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-if int % 3 == 0
-  "Fizz"
+if int % 3 == 0 && int % 5 == 0
+"FizzBuzz"
+elsif int % 3 == 0
+"Fizz"
 elsif int % 5 == 0
-  "Buzz"
-elsif int % 3 == 0 && int % 5 == 0
-  "FizzBuzz"
-else
-  return nil
-  end
+"Buzz"
+else 
+return nil
+end
 end
